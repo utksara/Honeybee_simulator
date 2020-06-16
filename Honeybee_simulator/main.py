9e9b59325feb2164fb969df362b4257961f48395
Utkarsh Saraswat <saraswatisdaniel@gmail.com>
2020-06-16 23:44:23 +0530

    simulated fireflies

--- Honeybee_simulator/main.py
+++ Honeybee_simulator/main.py
@@ -5,8 +5,11 @@ import numpy as np
 root = tkinter.Tk()
 
 image_home = './images/'
-size = 0
-canvas = tkinter.Canvas(root, width=1000, height=1000)
+canvas_dimensions = {'x':1000, 'y':600}
+
+no_of_bees = 3
+
+canvas = tkinter.Canvas(root, width=canvas_dimensions['x'], height=canvas_dimensions['y'])
 canvas.pack()
 
 label = tkinter.Label(root)
@@ -14,99 +17,81 @@ global_time = 0
 
 firefly_image = image_home + 'firefly.png'
 flower_image = image_home + 'flowers.png'
-background_image = [image_home + 'nightScene.png']
+background_image = image_home + 'nightScene.png'
 
 coordinates = []
 images_in_frame = []
 
+static_images = []
+dynamic_images = []
+phase_vector = np.random.randint(0,7,size = no_of_bees)
 
-scenery = []
+def create_images(image_file, x=500,y=500, scaling_factor = 1, type_ = 'static'):
+    global images_in_frame, static_images, dynamic_images
+    image = Image.open(image_file)
+    width, height = image.size
 
-def static_images(image_files):
-    global images_in_frame, scenery
-    for image_file in image_files:
-        image = Image.open(image_file)
-        image = image.resize((1000, 1000), Image.ANTIALIAS)
-        scenery.append(ImageTk.PhotoImage(image))
+    new_width = int(np.rint(scaling_factor*width))
+    new_height = int(np.rint(scaling_factor*height))
 
-static_images(background_image)
-canvas.create_image(500,500, image=scenery)
+    image = image.resize((new_width, new_height), Image.ANTIALIAS)
+    photoImage = ImageTk.PhotoImage(image)
+    images_in_frame.append(photoImage)
 
+    if(type_ =='static'):
+        static_images.append(photoImage)
+    else:
+        dynamic_images.append(photoImage)
+    canvas.create_image(x,y, image=photoImage)
 
-def generate_landing_movement(target, position):
-        global global_time
-        movement = (target-position)/10
-        return movement
+def landing_motion(target, position):
+    global global_time
+    movement = (target-position)/10
+    return movement
 
+def random_motion(step):
+    theta = np.random.randint(0, 360+90)
+    theta = theta * np.pi/180
+    y_step_random = step * 1 if np.sin(theta) > 0 else step* -1
+    x_step_random = step * 1 if np.cos(theta) > 0 else step* -1
+    movement = step*np.array([x_step_random,y_step_random]).T
+    del theta
+    return movement
 
-def generate_8_movement(step):
-        global global_time
-        ones = np.array([np.ones(2).tolist()]).T
-        theta = 30 * np.cos(global_time/400)
-        movement = np.matmul(ones, np.array([[np.sin(theta), np.cos(theta)]]))
-        movement = np.random.randint(0, 10)/10*step*movement
-        global_time += 1
-        del theta, ones
-        return movement
-
-
-def generate_random_movement(step):
-        movement = np.random.randint(-1000, 1000, size=2)/1000
-        ones = np.array([np.ones(2).tolist()]).T
-        denom = np.matmul(np.square(movement), ones)
-        denom = np.sqrt(denom)
-        movement = step*movement/denom
-        del ones, denom
-        return movement
-
-def create_image():
+def hovering_motion(current_position, center = np.array([500,500]), axis = 0, step = 1, period = 1, phase = 0):
+    global global_time
+    theta = global_time * np.pi/180
+    x_step_periodic = 500 + 400 * np.cos(phase + theta/(2*period))
+    y_step_periodic = 400 + 100 * np.sin(phase + theta/period)
+    movement = np.array([x_step_periodic,y_step_periodic])
+    return movement
     
-    global coordinates,images_in_frame, canvas    
-
+def simulate():
+    global coordinates,images_in_frame, canvas, static_images, dynamic_images, global_time  
     images_in_frame.clear()
+    dynamic_images.clear()
     label.config(image='')
-
-    for i in range(0,6):
-        movement = generate_random_movement(5)
-        coordinates[i] = np.abs(coordinates[i] + movement)
+    global_time = global_time + 1
+    for i in range(0,no_of_bees):
+        movement = hovering_motion(coordinates[i],step = 2, phase = phase_vector[i])
+        coordinates[i] = movement
         del movement
-
-    bee = ImageTk.PhotoImage(Image.open(firefly_image))
-    
-    image = Image.open(flower_image)
-    image = image.resize((100, 100), Image.ANTIALIAS)
-    flower = ImageTk.PhotoImage(image)
-
-    images_in_frame.append(bee)
-    images_in_frame.append(flower)
-    images_in_frame.append(scenery)
-
-    canvas.create_image(coordinates[6][0],coordinates[6][1], image=flower)
-    canvas.create_image(coordinates[7][0],coordinates[7][1], image=flower)
-    canvas.create_image(coordinates[8][0],coordinates[8][1], image=flower)
-
-    canvas.create_image(coordinates[0][0],coordinates[0][1], image=bee)
-    canvas.create_image(coordinates[1][0],coordinates[1][1], image=bee)
-    canvas.create_image(coordinates[2][0],coordinates[2][1], image=bee)
-    canvas.create_image(coordinates[3][0],coordinates[3][1], image=bee)
-    canvas.create_image(coordinates[4][0],coordinates[4][1], image=bee)
-    canvas.create_image(coordinates[5][0],coordinates[5][1], image=bee)
-
-    del flower, bee
+    images_in_frame = static_images + dynamic_images
+    for i in range(0,no_of_bees):
+        create_images(firefly_image, coordinates[i][0],coordinates[i][1], scaling_factor= 0.1, type_= 'dynamic')
     label.image = images_in_frame
     label.pack()
-
-    canvas.after(5,create_image)
+    canvas.after(5,simulate)
 
 def init_iteration():
     global coordinates
-    coordinates  = np.array([np.random.randint(500, size=2),np.random.randint(500, size=2),np.random.randint(500, size=2),
-                   np.random.randint(500, size=2),np.random.randint(500, size=2),np.random.randint(500, size=2),
-                   np.array([500,600]),np.array([700,600]),np.array([300,600])])
+    coordinates = np.array(np.random.randint(500, size=(no_of_bees,2)))
     
 def create_character():
     init_iteration()
-    canvas.after(1000,create_image)
+    canvas.after(1000,simulate)
 
+create_images(background_image, y=300, scaling_factor=1)
 create_character()
+
 root.mainloop()
